6044d6e01bcb8d37b73fab79bc23488c283d7ae8
acavegirl <airl627813@gmail.com>
2024-05-22 15:01:05 +0800

    refactor: useRef

--- src/pages/Lines.jsx
+++ src/pages/Lines.jsx
@@ -1,59 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, MeshPhongMaterial, DirectionalLight, Vector3, BufferGeometry, LineBasicMaterial, Line } from 'three';
 
+const pointsData = [[-10, 0, 0], [0, 10, 0], [10, 0, 0]]
+const points = pointsData.map(item => new Vector3(...item))
 
 export default () => {
   // 对dom的引用
   const canvasRef = useRef()
-  const resizeFnRef = useRef()
+
+  const scene = useRef(new Scene())
+
+  const geometry = useRef(new BufferGeometry().setFromPoints(points))
+  const material = useRef(new LineBasicMaterial({ color: 0x0000ff }))
+  const line = useRef(new Line(geometry.current, material.current))
+
+  const camera = useRef(new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000))
+
+  const renderer = useRef()
+  const resizeFnRef = useRef(() => {
+    // 自适应之后重新设置镜头
+    const canvas = renderer.current.domElement //获取 canvas
+    camera.current.aspect = canvas.clientWidth / canvas.clientHeight // 设置镜头宽高比，第二个参数
+    camera.current.updateProjectionMatrix() // 通知镜头更新视椎(视野)
+
+    renderer.current.setSize(canvas.clientWidth, canvas.clientHeight, false)
+    renderer.current.render(scene.current, camera.current)
+  })
+
+  useEffect(() => {
+    scene.current.add(line.current)
+
+    camera.current.position.set( 0, 0, 100 );
+    camera.current.lookAt( 0, 0, 0 );
+  }, [])
 
   useEffect(()=>{
     if (canvasRef.current){
-      // scene
-      const scene = new Scene()
+      renderer.current = new WebGLRenderer({ canvas: canvasRef.current })
+      renderer.current.render(scene.current, camera.current)
 
 
-      // geometry, material, line
-      const pointsData = [[-10, 0, 0], [0, 10, 0], [10, 0, 0]]
-      const points = pointsData.map(item => new Vector3(...item))
-      const geometry = new BufferGeometry().setFromPoints(points)
-
-      const material = new LineBasicMaterial({ color: 0x0000ff })
-
-      const line = new Line(geometry, material)
-      scene.add(line)
-
-
-      // camera
-      const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-      // 设置透视镜头的Z轴距离
-      // camera.position.z = 20;
-      camera.position.set( 0, 0, 100 );
-      camera.lookAt( 0, 0, 0 );
-
-      // renderer
-      const renderer = new WebGLRenderer({ canvas: canvasRef.current })
-      renderer.render(scene, camera)
-
-      const resizeFn = () => {
-        // console.log("resizeFn")
-        // 自适应之后重新设置镜头
-        const canvas = renderer.domElement //获取 canvas
-        camera.aspect = canvas.clientWidth / canvas.clientHeight // 设置镜头宽高比，第二个参数
-        camera.updateProjectionMatrix() // 通知镜头更新视椎(视野)
-
-        // 修改渲染尺寸大小，第3个参数为可选参数，默认值为 true，false 意思是阻止因渲染内容尺寸发生变化而去修改 canvas 尺寸
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false)
-        renderer.render(scene, camera)
-      }
-
-      resizeFn()
-      resizeFnRef.current = resizeFn
-
-      renderer.render(scene, camera)
-
-
-      window.addEventListener('resize', resizeFn) //添加窗口 resize 事件处理函数
+      window.addEventListener('resize', resizeFnRef.current) //添加窗口 resize 事件处理函数
       return () => {
         if (resizeFnRef.current) {
           window.removeEventListener('resize', resizeFnRef.current)
